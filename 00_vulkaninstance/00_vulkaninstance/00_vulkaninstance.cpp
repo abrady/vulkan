1b74dd8452f1ea32d3e23ae7dd9e9ad269525c37
abrady <abrady@meta.com>
2023-09-19 21:58:50 -0700

    init working

--- 00_vulkaninstance/00_vulkaninstance/00_vulkaninstance.cpp
+++ 00_vulkaninstance/00_vulkaninstance/00_vulkaninstance.cpp
@@ -6,8 +6,10 @@
 #include "vulkan/vulkan.hpp"
 
 static int validation_error = 0;
+#define APP_SHORT_NAME "vulkaninstance"
 
 class MyVulkan {
+    vk::Instance inst;
 public:
     void init_vk() {
         // todo: validation layers
@@ -20,6 +22,32 @@ public:
             &debug_messenger_callback, static_cast<void*>(this));
 
 
+        auto const app = vk::ApplicationInfo()
+            .setPApplicationName(APP_SHORT_NAME)
+            .setApplicationVersion(0)
+            .setPEngineName(APP_SHORT_NAME)
+            .setEngineVersion(0)
+            .setApiVersion(VK_API_VERSION_1_0);
+
+        // is a Vulkan layer used for debuggingand validation of Vulkan API usage in your application.This layer helps to identify various types of errors, warnings, 
+        // and performance issues in Vulkan applications by checking the correctness of the API usage according to the Vulkan specification.
+        std::vector<const char*> enabled_layers = { "VK_LAYER_KHRONOS_validation" };
+        std::vector<const char*> enabled_instance_extensions = {
+            VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME,
+            VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+            VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+            VK_KHR_SURFACE_EXTENSION_NAME,
+        };
+
+        auto const inst_info = vk::InstanceCreateInfo()
+            // something something backward compat?
+            //.setFlags(portabilityEnumerationActive ? vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR
+            //    : static_cast<vk::InstanceCreateFlagBits>(0))
+            .setPNext(&debug_utils_create_info)
+            .setPApplicationInfo(&app)
+            .setPEnabledLayerNames(enabled_layers)
+            .setPEnabledExtensionNames(enabled_instance_extensions);
+        this->inst = vk::createInstance(inst_info);
     }
 
     static VKAPI_ATTR VkBool32 VKAPI_CALL debug_messenger_callback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
@@ -84,7 +112,10 @@ public:
 
 
 int main() {
-    std::cout << "hello world!\n";
+    std::cout << "initializing vulkan!\n";
+    MyVulkan mv;
+    mv.init_vk();
+    std::cout << "done!\n";
     return validation_error;
 }
 
